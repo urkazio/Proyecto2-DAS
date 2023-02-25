322f5864eec520ca5b4d045add42bff6ad11bd77
Urko <urkoupv@gmail.com>
2023-02-25 22:58:10 +0100

    creadas las 5 pantallas de login y sus interfaces

--- app/src/main/java/com/example/pruebamonigote/ActividadLogin4.java
+++ app/src/main/java/com/example/pruebamonigote/ActividadLogin4.java
@@ -3,12 +3,33 @@ package com.example.pruebamonigote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class ActividadLogin4 extends AppCompatActivity {
+public class ActividadLogin4 extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_login4);
+
+        //combobox
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.genero, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
