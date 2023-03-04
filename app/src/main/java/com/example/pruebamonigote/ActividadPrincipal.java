86b052938af5f75599c4c39c676dee97bf0655fe
Urko <urkoupv@gmail.com>
2023-03-04 13:12:19 +0100

    completada Casi la la funcionalidad 2/3 de mi app añadida la notifiacion de crear rutina y pulido el dialogo entero
    Falta poder escribir en el fichero ya que da error

--- app/src/main/java/com/example/pruebamonigote/ActividadPrincipal.java
+++ app/src/main/java/com/example/pruebamonigote/ActividadPrincipal.java
@@ -1,12 +1,16 @@
 package com.example.pruebamonigote;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -18,6 +22,11 @@ public class ActividadPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actividad_principal);
+
+        // importar android Manifest
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS)!= PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+        }
     }
     public void explorar(View v) {
         Intent intent = new Intent(this, ActividadListaRutinasPred.class);
@@ -26,7 +35,8 @@ public class ActividadPrincipal extends AppCompatActivity {
     public void crear(View v) {
         //DialogFragment dialogoalerta= new DialogoCrearRutina();
         //dialogoalerta.show(getSupportFragmentManager(), "dialogoCrearRutina");
-        DialogoCrearRutina.onCreateDialog(this,v);
+        DialogoCrearRutina dR = new DialogoCrearRutina();
+        dR.onCreateDialog(this, v);
 
 
     }
--- app/src/main/java/com/example/pruebamonigote/DialogoCrearRutina.java
+++ app/src/main/java/com/example/pruebamonigote/DialogoCrearRutina.java
@@ -1,47 +1,96 @@
 package com.example.pruebamonigote;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
 import android.text.InputFilter;
+import android.text.InputType;
+import android.text.Spanned;
+import android.text.method.DigitsKeyListener;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import android.Manifest;
+
 public class DialogoCrearRutina {
-    public static void onCreateDialog(ActividadPrincipal actividadPrincipal, View v) {
+    public void onCreateDialog(ActividadPrincipal actividadPrincipal, View v) {
 
         AlertDialog.Builder dialog = new AlertDialog.Builder(actividadPrincipal);
 
         Context context = actividadPrincipal;
 
+        /**####################################################
+         ######################   layout    ###################
+         ####################################################**/
         // Layout propio para el dialogo
         LinearLayout layout = new LinearLayout(context);
         layout.setOrientation(LinearLayout.VERTICAL);
 
-        //el id de la rutina será el num lineas del fichero misRutinas
-
         dialog.setTitle("   Nueva Rutina Vacía");
+        dialog.setIcon(R.drawable.logo);
+        dialog.setCancelable(false);
 
+
+        /**####################################################
+         ################   titulo y editable    ##############
+         ####################################################**/
         // Añadir un TextView para indicar que se debe insertar el "Título"
         final TextView tvNombre = new TextView(context);
-        tvNombre.setText("Introduce el nombre de la rutina");
+        tvNombre.setText("Introduce el nombre de la rutina:");
         tvNombre.setPadding(100,80,100,20);
         layout.addView(tvNombre); // Se añade a la vista
 
         // Añadir un EditText para insertar el "Título"
         final EditText titleBox = new EditText(context);
         titleBox.setHint("Nombre");
+        titleBox.setFilters(new InputFilter[] { new InputFilter.LengthFilter(12) }); //limitar a 12 caracteres
         titleBox.setPadding(100,20,100,40);
-        layout.addView(titleBox); // Se añade a la vista
+        //añadir restriccion para solo permitir caracteres a-z y A-Z
+        titleBox.setFilters(new InputFilter[]{
+                new InputFilter() {
+                    public CharSequence filter(CharSequence src, int start,
+                                               int end, Spanned dst, int dstart, int dend) {
+                        if (src.equals("")) {
+                            return src;
+                        }
+                        if (src.toString().matches("[a-zA-Z ]+")) {
+                            return src;
+                        }
+                        return "";
+                    }
+                }
+        });
+        layout.addView(titleBox); // Por ultimo se añade a la vista
 
+
+        /**####################################################
+         #############   descripcion y editable    ############
+         ####################################################**/
         // Añadir otro TextView para indicar que se debe insertar el "Descripción"
         final TextView tvDescr = new TextView(context);
-        tvDescr.setText("Añade una breve descripción a la rutina (máx 50 caracteres)");
+        tvDescr.setText("Añade una breve descripción a la rutina (máx 50 caracteres):");
         tvDescr.setPadding(100,80,100,20);
         layout.addView(tvDescr); // Se añade a la vista
 
@@ -50,24 +99,98 @@ public class DialogoCrearRutina {
         descriptionBox.setFilters(new InputFilter[] { new InputFilter.LengthFilter(50) }); //limitar a 50 caracteres
         descriptionBox.setHint("Descripción");
         descriptionBox.setPadding(100,20,100,40);
-        layout.addView(descriptionBox); // Se añade a la vista
+        //añadir restriccion para solo permitir caracteres a-z y A-Z
+        descriptionBox.setFilters(new InputFilter[]{
+                new InputFilter() {
+                    public CharSequence filter(CharSequence src, int start,
+                                               int end, Spanned dst, int dstart, int dend) {
+                        if (src.equals("")) {
+                            return src;
+                        }
+                        if (src.toString().matches("[a-zA-Z ]+")) {
+                            return src;
+                        }
+                        return "";
+                    }
+                }
+        });
+        layout.addView(descriptionBox); // Por ultimo se añade a la vista
 
-        dialog.setIcon(R.drawable.logo);
 
-        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        /**####################################################
+         #############   botones y listeners    ###############
+         ####################################################**/
+        dialog.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                //m_Text = input.getText().toString();
+                String nombreRut = titleBox.getText().toString();
+                System.out.println(nombreRut);
+                String descripcionRut = titleBox.getText().toString();
+
+                //escribir en el fichero "misrutinas.txt" la nueva rutina creada por el usuario
+                try {
+                    //el id de la rutina será el num lineas del fichero misRutinas
+                    OutputStreamWriter fichero = new OutputStreamWriter(context.openFileOutput("misrutinas.txt", Context.MODE_PRIVATE));
+                    fichero.write("Estoy escribiendo en el fichero");
+                    System.out.println(nombreRut);
+                    fichero.close();
+
+
+                    /**###################################################################
+                    //###  Gestion de notificaciones (para cuando se crea una rutina)  ###
+                    //#################################################################**/
+
+                    NotificationManager elManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                    NotificationCompat.Builder builderNotifi = new NotificationCompat.Builder(context, "1");
+
+                    //definicion para el canal
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        NotificationChannel elCanal = new NotificationChannel("1", "haGanado", NotificationManager.IMPORTANCE_DEFAULT);
+                        //configuracion del canal
+                        elCanal.setDescription("Descripción del canal");
+                        elCanal.enableLights(true);
+                        elCanal.setLightColor(Color.RED);
+                        elManager.createNotificationChannel(elCanal);
+                    }
+
+                    //Definicion de la alerta personalizada al ganar
+                    builderNotifi.setLargeIcon(BitmapFactory.decodeResource(context.getResources(),R.drawable.logo))
+                            .setSmallIcon(R.drawable.logo)
+                            .setContentTitle("Notificacion de Fit Pro")
+                            .setSubText("Nueva rutina vacía")
+                            .setContentText("Enhorabuena! Has creado la nueva rutina " +nombreRut+ " con exito!!!")
+                            .setAutoCancel(true);
+
+
+                    //recuperacion de contexto (id de la notifi) para luego poder cerrarla
+                    Bundle extras = ((Activity) context).getIntent().getExtras();
+                    if (extras != null) {
+                        int id = extras.getInt("id");
+                        elManager.cancel(id);
+                    }
+
+                    //POR ULTIMO: lanzar la notificacion
+                    elManager.notify(1,builderNotifi.build());
+
+                    /** ##################### fin de gestion de notifiacion #########################**/
+
+
+                } catch (IOException e){
+                    System.out.println("Expecepcion");
+                }
+
             }
         });
-        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+
+        dialog.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
             }
         });
 
-        dialog.setView(layout); // Again this is a set method, not add
-        dialog.show();
+        dialog.setView(layout); // Añadir el layout completo del dialogo al dialogo
+        dialog.show(); // Por uultimo, mostrarlo.
     }
 }
